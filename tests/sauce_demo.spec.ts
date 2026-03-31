cf5d008d2fd5ea4ac5c919c64531cd62da60657c
herebus1 <33296126+herebus1@users.noreply.github.com>
2026-03-30 19:42:22 -0500

    Flujo completo carrito de compra

---
+++ tests/sauce_demo.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test('Login', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+
+    const items = await page.locator('#inventory_container .inventory_item').all();
+    const randomItem = items[Math.floor(Math.random() * items.length)];
+    const itemName = await randomItem.locator('.inventory_item_name').innerText();
+    const itemDesc = await randomItem.locator('.inventory_item_desc').innerText();
+    const itemPrice = await randomItem.locator('.inventory_item_price').innerText();
+    console.log(`Item Name: ${itemName}, Item Description: ${itemDesc}, Item Price: ${itemPrice}`);
+    await randomItem.getByRole('button', { name: 'Add to cart' }).click();
+    await page.locator('.shopping_cart_link').click();
+    await expect(page.getByRole('button', { name: 'Checkout' })).toBeVisible();
+    const cartItemName = await page.locator('.inventory_item_name').innerText();
+    const cartItemDesc = await page.locator('.inventory_item_desc').innerText();
+    const cartItemPrice = await page.locator('.inventory_item_price').innerText();
+    expect(cartItemName).toEqual(itemName);
+    expect(cartItemDesc).toEqual(itemDesc);
+    expect(cartItemPrice).toEqual(itemPrice);
+    //await page.pause();
+    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    await page.getByRole('textbox', { name: 'First Name' }).fill('John');
+    await page.getByRole('textbox', { name: 'Last Name' }).fill('Doe');
+    await page.getByRole('textbox', { name: 'Zip/Postal Code' }).fill('12345');
+    await page.getByRole('button', { name: 'Continue' }).click();
+
+    const checkoutItemName = await page.locator('.inventory_item_name').innerText();
+    const checkoutItemDesc = await page.locator('.inventory_item_desc').innerText();
+    const checkoutItemPrice = await page.locator('.inventory_item_price').innerText();
+    expect(checkoutItemName).toEqual(itemName);
+    expect(checkoutItemDesc).toEqual(itemDesc);
+    expect(checkoutItemPrice).toEqual(itemPrice);
+    await page.getByRole('button', { name: 'Finish' }).click();
+    await expect(page.getByRole('heading', { name: 'Thank you for your order!' })).toBeVisible();
+});
